df17ad21a9a8bc114523770c0da55d720488bbbb
tereyka <tereyka@mail.ru>
2025-10-21 12:09:36 +0300

    Поправила комментарии и лайки, чтобы они были на одной линии с иконками и увеличила размер шрифта количества лайков для лучшей читаемости.

--- js/likes.js
+++ js/likes.js
@@ -1,13 +1,35 @@
-let heart = document.querySelector('.heart');
-let likes_number = document.querySelector('.likes_number');
+// let heart = document.querySelector('.heart');
+// let likes_number = document.querySelector('.likes_number');
 
-heart.addEventListener('click', (e) => {
+// heart.addEventListener('click', (e) => {
+//     if (heart.classList.contains('heart_added')) {
+//         likes_number.textContent = Number(likes_number.textContent) - 1;
+//     } else {
+//         likes_number.textContent = Number(likes_number.textContent) + 1;
+//     }
+//     e.preventDefault();   // блокируем переход
+//     e.stopPropagation();  // останавливаем клик по карточке
+//     heart.classList.toggle('heart_added');
+// });
+
+
+
+const hearts = document.querySelectorAll('.heart');
+const likesNumbers = document.querySelectorAll('.likes_number');
+
+hearts.forEach((heart, index) => {
+  heart.addEventListener('click', (e) => {
+    // текущий счётчик для этой карточки
+    const likes_number = likesNumbers[index];
+
     if (heart.classList.contains('heart_added')) {
-        likes_number.textContent = Number(likes_number.textContent) - 1;
+      likes_number.textContent = Number(likes_number.textContent) - 1;
     } else {
-        likes_number.textContent = Number(likes_number.textContent) + 1;
+      likes_number.textContent = Number(likes_number.textContent) + 1;
     }
-    e.preventDefault();   // блокируем переход
-    e.stopPropagation();  // останавливаем клик по карточке
+
+    e.preventDefault();
+    e.stopPropagation();
     heart.classList.toggle('heart_added');
+  });
 });
