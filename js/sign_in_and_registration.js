4da9ad8bb8c3cd39c575977a0f121fdac3590a05
tereyka <tereyka@mail.ru>
2025-10-28 03:18:33 +0300

    Валидация формы

--- js/sign_in_and_registration.js
+++ js/sign_in_and_registration.js
@@ -38,16 +38,64 @@ document.addEventListener('click', (e) => {
   const modalReg = document.querySelector('.registration_modal');
   const signinCloseBtn = document.querySelector('.signin_close_btn');
   const registrationCloseBtn = document.querySelector('.registration_close_btn');
+  // const registrationFormBtn = document.querySelector('.registration-form-btn')
 
   // Если клик был по .signin_modal, а не по внутреннему блоку — закрыть
-  if (t === modalSignIn || t ===  signinCloseBtn) {
+  if (t === modalSignIn || t === signinCloseBtn) {
     modalSignIn.style.display = 'none';
     return;
   }
 
   // Если клик был по .registration_modal, а не по внутреннему блоку — закрыть
-  if (t === modalReg  || t === registrationCloseBtn) {
+  if (t === modalReg || t === registrationCloseBtn) {
     modalReg.style.display = 'none';
     return;
+  }
+
+  if (t.closest('.registration-form-btn')) {
+    const form = document.querySelector('form');
+    const email = document.getElementById('registration-email');
+    const emailErr = document.querySelector('.email-error');
+    const password = document.getElementById('registration-password');
+    const passwordErr = document.querySelector('.password-error');
+    const confirmPwd = document.getElementById('password-confirm');
+    const confirmErr = document.querySelector('.password-confirm-error');
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      email.classList.remove('invalid');
+      emailErr.classList.remove('active');
+
+      const emailValue = email.value.trim();
+
+      if (!emailValue.includes('@')) {
+        email.classList.add('invalid');
+        emailErr.classList.add('active');
+        return;
+      }
+
+      password.classList.remove('invalid');
+      passwordErr.classList.remove('active');
+
+      const passwordValue = password.value.trim();
+
+      if (passwordValue.length < 8) {
+        password.classList.add('invalid');
+        passwordErr.classList.add('active');
+        return;
+      }
+
+     confirmPwd.classList.remove('invalid');
+     confirmErr.classList.remove('active');
+
+    const confirmPwdValue = confirmPwd.value.trim();
+
+     if (passwordValue !== confirmPwdValue) {
+        confirmPwd.classList.add('invalid');
+       confirmErr.classList.add('active');
+        return;
+      }
+    })
   }
 });
